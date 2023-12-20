01b7b5002c9a508337783be0d6fc3a3f22582d9f
Christian Olivares <christianolivares@MacBook-Air-de-Christian.local>
2023-12-19 22:15:21 -0600

    se corrigen los slideshow

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css'
+import SlideShow from './components/SlideShow.jsx'
 import AboutMe from './components/aboutme.jsx'
 import Clients from './components/clients.jsx'
 import Footer from './components/footer.jsx'
 import Header from './components/header.jsx'
 import Navbar from './components/navbar.jsx'
-import SlideShow from './components/SlideShow.jsx'
 
 
 
--- src/components/SlideShow.jsx
+++ src/components/SlideShow.jsx
@@ -1,40 +1,40 @@
 import React, { useState } from 'react';
-import images from '../assets/images/images.js'; // Ajusta la ruta según tu estructura de archivos
-
+import images from '../assets/images/images.js';
+import { useMediaQuery } from 'react-responsive';
 
 const IMAGES_PER_SLIDE = 3;
 
 const SlideShow = () => {
+  const isMobile = useMediaQuery({ maxWidth: 767 }); // Ajusta el ancho máximo según tus necesidades
   const [startIndex, setStartIndex] = useState(0);
 
   const nextSlide = () => {
-    setStartIndex((prevIndex) => (prevIndex + IMAGES_PER_SLIDE) % Object.keys(images).length);
+    setStartIndex((prevIndex) => (prevIndex + (isMobile ? 1 : IMAGES_PER_SLIDE)) % Object.keys(images).length);
   };
 
   const prevSlide = () => {
     setStartIndex((prevIndex) =>
-      (prevIndex - IMAGES_PER_SLIDE + Object.keys(images).length) % Object.keys(images).length
+      (prevIndex - (isMobile ? 1 : IMAGES_PER_SLIDE) + Object.keys(images).length) % Object.keys(images).length
     );
   };
 
-  const visibleImages = Object.values(images).slice(startIndex, startIndex + IMAGES_PER_SLIDE);
-
+  const visibleImages = Object.values(images).slice(startIndex, startIndex + (isMobile ? 1 : IMAGES_PER_SLIDE));
 
   return (
     <div className='main-contai'>
-    <div className="slideshow-container">
-      {visibleImages.map((image, index) => (
-        <div key={index} className="slide">
-          <img className="slide-img"src={image} alt={`Slide ${startIndex + index + 1}`} />
-        </div>
-      ))}
-      <button className="prev" onClick={prevSlide}>
-        &#10094;
-      </button>
-      <button className="next" onClick={nextSlide}>
-        &#10095;
-      </button>
-    </div>
+      <div className="slideshow-container">
+        {visibleImages.map((image, index) => (
+          <div key={index} className="slide">
+            <img className="slide-img" src={image} alt={`Slide ${startIndex + index + 1}`} />
+          </div>
+        ))}
+        <button className="prev" onClick={prevSlide}>
+          &#10094;
+        </button>
+        <button className="next" onClick={nextSlide}>
+          &#10095;
+        </button>
+      </div>
     </div>
   );
 };
